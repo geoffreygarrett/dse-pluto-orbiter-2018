fbd2c2181d738d03b0bfca7c293e9672add427b1
Unknown <g.h.garrett13@gmail.com>
2018-05-30 11:37:29 +0200

    Fixed Lambert Solver

--- trajectory_tool/core.py
+++ trajectory_tool/core.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+from poliastro.plotting import OrbitPlotter
 from poliastro.bodies import Earth, Mars, Sun, Pluto, Venus, Jupiter
 from poliastro.twobody import Orbit
 from poliastro import iod
@@ -11,10 +12,39 @@ from astropy import time
 import numpy as np
 import datetime
 from collections import namedtuple
-from trajectory_tool import *
+lambert_parameters = namedtuple('lambert_parameters', 'r0 r1 v0 v1 tof attractor epoch0 epoch1 ss0 ss1')
 
 plt.style.use("seaborn")
 solar_system_ephemeris.set("jpl")
+
+#
+# # DECORATORS ---------------------------------------------------------------------------------------------------------
+# def decorators(lambert_hohman=True):
+#     class _Decorator(object):
+#
+#         def __init__(self, decorated):
+#             self._decorated = decorated
+#
+#         def __get__(self, instance, owner):
+#             self.cls = owner
+#             self.obj = instance
+#             return self.__call__
+#
+#         # Decorator 1
+#         def _lambert_hohman(self):
+#
+#
+#         def __call__(self, *args, **kwargs):
+#             if lambert_hohman is True:
+#                 self._handle_traffic()
+#
+#             if update_signature is True:
+#                 self._update_signature()
+#
+#     return _Decorator
+
+# def lambert_hohman(lambert_function):
+#     soln = lambert_function(*args, **kwargs)
 
 
 # CORE CLASS -----------------------------------------------------------------------------------------------------------
@@ -37,7 +67,7 @@ class TrajectoryTool(object):
         (v0, v) = iod.lambert(main_attractor.K, r0, r, tof)
 
         return lambert_parameters(r0=r0, r1=r, v0=v0, v1=v, tof=tof,  attractor=main_attractor, epoch0=epoch0,
-                                  epoch1=epoch1)
+                                  epoch1=epoch1, ss0=None, ss1=None)
 
     @staticmethod
     def lambert_solve_from_orbits(body1_orbit_with_epoch,  body2_orbit_with_epoch,  main_attractor=Sun):
@@ -57,7 +87,7 @@ class TrajectoryTool(object):
         (v0, v) = iod.lambert(main_attractor.k, ss0.r, ss1.r, time_of_flight)       # Output parameters (v0, v)
 
         return lambert_parameters(r0=r0, r1=r, v0=v0, v1=v, tof=time_of_flight, attractor=main_attractor,
-                                  epoch0=ss0.epoch, epoch1=ss1.epoch)
+                                  epoch0=ss0.epoch, epoch1=ss1.epoch, ss0=ss0, ss1=ss1)
 
     @staticmethod
     def lambert_solve_from_bodies(body1, body2, epoch1, epoch2, main_attractor=Sun):
@@ -74,12 +104,16 @@ class TrajectoryTool(object):
         ss0 = Orbit.from_body_ephem(body1, epoch1)
         ss1 = Orbit.from_body_ephem(body2, epoch2)
 
-        time_of_flight = epoch2 - epoch1                                              # Input parameter TOF
-        (r0, r) = ss0.r, ss1.r                                                        # Input parameters  (r0, r)
+        time_of_flight = epoch2 - epoch1                                               # Input parameter TOF
         (v0, v), = iod.lambert(main_attractor.k, ss0.r, ss1.r, time_of_flight)         # Output parameters (v0, v)
 
-        return lambert_parameters(r0=r0, r=r, v0=v0, v=v, tof=time_of_flight, attractor=main_attractor,
-                                  epoch0=ss0.epoch, epoch1=ss1.epoch)
+        return lambert_parameters(r0=ss0.r, r1=ss1.r, v0=v0, v1=v, tof=time_of_flight, attractor=main_attractor,
+                                  epoch0=ss0.epoch, epoch1=ss1.epoch, ss0=ss0, ss1=ss1)
+
+    def lambert_hohman_solution(self, *args, **kwargs):
+        solution = self.lambert_solve_from_bodies(*args, **kwargs)
+
+        yield
 
     def __init__(self):
         self.x = 0
@@ -88,15 +122,18 @@ class TrajectoryTool(object):
 if __name__ == '__main__':
     test = TrajectoryTool()
 
-    date_launch = time.Time('2011-11-26 15:02', scale='utc')
-    date_arrival = time.Time('2012-08-06 05:17', scale='utc')
+    date_launch = time.Time('2018-05-30 15:02', scale='utc')
+    date_arrival = time.Time('2021-05-30 05:17', scale='utc')
 
-    lambert_solution = test.lambert_solve_from_bodies(body1=Earth, body2=Mars, epoch1=date_launch,
+    lambert_solution = test.lambert_solve_from_bodies(body1=Earth, body2=Jupiter, epoch1=date_launch,
                                                       epoch2=date_arrival)
 
     test_orbit = Orbit.from_vectors(lambert_solution.attractor, lambert_solution.r0, lambert_solution.v0,
                                     lambert_solution.epoch0)
 
+    op = OrbitPlotter()
+    op.plot(lambert_solution.ss1)
+    op.plot(lambert_solution.ss0)
+    op.plot(test_orbit)
 
-
-
+    plt.show()
