f75fee9b7f70563ad9b700364877fd4607ed1ed2
Unknown <matthew.turnock1@gmail.com>
2018-06-04 09:13:13 +0200

    Gravity ass minor update

--- trajectory_tool/grav_ass/Hyperbolic_calculator_resources_v0.3.py
+++ trajectory_tool/grav_ass/Hyperbolic_calculator_resources_v0.3.py
@@ -127,23 +127,23 @@ def grav_ass(v_in, v_bod, v_out_req, d, mu, mode):
 # #======================================================================================================================================
 # # TEST CASE
 # #======================================================================================================================================
-import numpy as np
-import core
-import matplotlib.pyplot as plt
-from matplotlib import cm
-
-from poliastro.plotting import OrbitPlotter
-from poliastro.bodies import Earth, Mars, Sun, Pluto, Venus, Jupiter
-from poliastro.twobody import Orbit
-from poliastro import iod
-from astropy.coordinates import solar_system_ephemeris
-from astropy import time
-import math
-from astropy import units as u
-
-import datetime
-from collections import namedtuple
-
+# import numpy as np
+# import core
+# import matplotlib.pyplot as plt
+# from matplotlib import cm
+#
+# from poliastro.plotting import OrbitPlotter
+# from poliastro.bodies import Earth, Mars, Sun, Pluto, Venus, Jupiter
+# from poliastro.twobody import Orbit
+# from poliastro import iod
+# from astropy.coordinates import solar_system_ephemeris
+# from astropy import time
+# import math
+# from astropy import units as u
+#
+# import datetime
+# from collections import namedtuple
+#
 
 # G = 6.67408*10**-11*10**-9 #km^3/kg/s^2
 # AU = 1.496E8                  #km
