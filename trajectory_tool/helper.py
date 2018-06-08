465af9d56b94c971d76d886791ce68e862f538f6
Unknown <matthew.turnock1@gmail.com>
2018-06-08 16:27:06 +0200

    Updated miss distance .json

--- trajectory_tool/helper.py
+++ trajectory_tool/helper.py
@@ -6,46 +6,53 @@
 #     return np.linalg.norm(x2-x1)
 
 factor = {
-    'mercury':
+    'mercury':1.082
+    'venus': 1.047
+    'earth': 1.048
+    'mars': 1.076
     'jupiter': 1.6
+    'saturn': 1.342
+    'uranus': 4.19
+    'neptune': 1.181
+    'pluto': 9.34
 }
 
 
 body_d_domain = {
           'mercury':{
                             'upper':  1.12E5,
-                            'lower': factor['mercury'] * 2539.7
+                            'lower': factor['mercury'] * 2440
   },
       'venus':{
                             'upper': 6.16E5,
-                            'lower': 6427.67
+                            'lower': factor['venus'] * 2440
   },
       'earth':{
                             'upper': 9.25E5,
-                            'lower': 6540.7568
+                            'lower': factor['earth'] * 2440
   },
       'mars':{
                             'upper': 5.76E5,
-                            'lower': 3552.2672
+                            'lower': factor['mars'] * 2440
   },
       'jupiter':{
                             'upper': 4.82E7,
-                            'lower': 72008.182
+                            'lower': factor['jupiter'] * 2440
   },
       'saturn':{
                             'upper': 5.48E7,
-                            'lower': 61405.511
+                            'lower': factor['saturn'] * 2440
   },
       'uranus':{
                             'upper': 5.18E7,
-                            'lower': 26088.564
+                            'lower': factor['uranus'] * 2440
   },
       'neptune':{
                             'upper': 8.66E7,
-                            'lower': 25152.092
+                            'lower': factor['neptune'] * 2440
   },
       'pluto':{
                             'upper': 3.16E6,
-                            'lower': 1287
+                            'lower': factor['pluto'] * 2440
   }
 }
