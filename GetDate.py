2ea3fb0c36d0d4c401471c13c840ee4f638c19f7
Unknown <matthew.turnock1@gmail.com>
2018-06-11 09:52:08 +0200

    GetDateForAndy

--- GetDate.py
+++ GetDate.py
@@ -1,8 +1,16 @@
 
 from trajectory_tool.ale_solar_system.solar_system import SolarSystem
-
-
-
+import datetime
+import time
+import numpy as np
 
 
 ss = SolarSystem()
+
+epoch = 2471151.500000
+
+
+# use this http://aa.usno.navy.mil/data/docs/JulianDate.php
+this = ss.coordinates_of('PLUTO', epoch)
+distance = np.linalg.norm(this)
+print(distance, 'km')
